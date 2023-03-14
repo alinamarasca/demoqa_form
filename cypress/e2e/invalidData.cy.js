47270808d720fc52b5423afc2c8c46b3f2cf636d
Alina Marasca <75176258+alinamarasca@users.noreply.github.com>
2023-03-14 08:57:23 +0100

    all foreach

--- cypress/e2e/invalidData.cy.js
+++ cypress/e2e/invalidData.cy.js
@@ -6,34 +6,32 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 });
 
 import { commonObject as c } from "../page_objects/commonObject";
+import { invalidData as d } from "./invalidData";
 
 describe("fill the form", () => {
   context("invalid data in required fields", () => {
-    before(() => {
-      cy.fixture("invalidData").then(function (data) {
-        this.d = data;
-      });
+    beforeEach(() => {
       cy.visit("/");
       cy.get(c.pageEl.categoryCard).contains(c.pageEl.formsCard).click();
       cy.get(c.pageEl.menuItemForms).contains("Practice Form").click();
     });
-
-    it("first name", function () {
-      this.d.firstName.forEach(input => {
+    d.firstName.forEach(input => {
+      it(`first name, value: "${input}" `, () => {
+        cy.get(c.formEl.firstNameInput).invoke("val", input);
+        cy.get(c.formEl.studentForm).submit();
+        cy.checkInputValidity(c.formEl.firstNameInput, false);
+      });
+    });
+    d.lastName.forEach(input => {
+      it(`last name, value: "${input}"`, () => {
         cy.get(c.formEl.firstNameInput).invoke("val", input);
         cy.get(c.formEl.studentForm).submit();
         cy.checkInputValidity(c.formEl.firstNameInput, false);
       });
     }),
-      it("last name", function () {
-        this.d.lastName.forEach(input => {
-          cy.get(c.formEl.lastNameInput).invoke("val", input);
-          cy.get(c.formEl.studentForm).submit();
-          cy.checkInputValidity(c.formEl.lastNameInput, false);
-        });
-      }),
-      it("email", function () {
-        this.d.lastName.forEach(input => {
+      d.email.forEach(input => {
+        it(`email, value: "${input}"`, () => {
+          cy.log(input);
           cy.get(c.formEl.emailInput).invoke("val", input);
           cy.get(c.formEl.studentForm).submit();
           cy.checkInputValidity(c.formEl.emailInput, false);
@@ -45,19 +43,24 @@ describe("fill the form", () => {
           cy.checkInputValidity(c.formEl.inlineRadio, false);
         });
       }),
-      it("phone", function () {
-        this.d.phone.forEach(input => {
+      d.phone.forEach(input => {
+        it(`phone, value: "${input}"`, () => {
           cy.get(c.formEl.mobilePhoneInput).invoke("val", input);
           cy.get(c.formEl.studentForm).submit();
           cy.checkInputValidity(c.formEl.mobilePhoneInput, false);
         });
       }),
-      it("address", function () {
-        this.d.address.forEach(input => {
-          cy.get(c.formEl.addressInput).invoke("val", input);
-          cy.get(c.formEl.studentForm).submit();
-          cy.checkInputValidity(c.formEl.addressInput, false);
-        });
+      it("phone trims input to 10 digits", () => {
+        cy.get(c.formEl.mobilePhoneInput).focus().type("00001234567");
+        cy.get(c.formEl.mobilePhoneInput).should("have.value", "0000123456");
       });
+
+    d.address.forEach(input => {
+      it(`address, value: "${input}"`, () => {
+        cy.get(c.formEl.currentAddressInput).invoke("val", input);
+        cy.get(c.formEl.studentForm).submit();
+        cy.checkInputValidity(c.formEl.currentAddressInput, false);
+      });
+    });
   });
 });
---
+++ cypress/e2e/invalidData.js
@@ -0,0 +1,7 @@
+export const invalidData = {
+  firstName: [""],
+  lastName: [""],
+  email: ["", " ", "invalidemail.com", "invalid@", ".com", 789],
+  phone: ["", " ", "01234567", "aloha"],
+  address: [""]
+};
